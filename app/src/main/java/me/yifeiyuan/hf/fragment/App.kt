0002f3ad827a54aedabb360e97e96fe8a659e86b
zhening.czn <zhening.czn@alibaba-inc.com>
2021-01-15 14:55:06 +0800

    init

---
+++ app/src/main/java/me/yifeiyuan/hf/fragment/App.kt
@@ -0,0 +1,17 @@
+package me.yifeiyuan.hf.fragment
+
+import android.app.Application
+
+/**
+ * Created by 程序亦非猿 on 2021/1/15.
+ */
+class App : Application() {
+
+
+    override fun onCreate() {
+        super.onCreate()
+
+
+    }
+
+}
