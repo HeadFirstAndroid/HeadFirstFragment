a6f12153f22583b10a2cd1b359f4dd527193cbae
zhening.czn <zhening.czn@alibaba-inc.com>
2021-01-15 16:44:10 +0800

    update

--- app/src/main/java/me/yifeiyuan/hf/fragment/App.kt
+++ app/src/main/java/me/yifeiyuan/hf/fragment/App.kt
@@ -1,17 +1,26 @@
 package me.yifeiyuan.hf.fragment
 
 import android.app.Application
+import me.yifeiyuan.adh.DebugConfig
+import me.yifeiyuan.adh.DebugHelper
 
 /**
  * Created by 程序亦非猿 on 2021/1/15.
  */
 class App : Application() {
 
-
     override fun onCreate() {
         super.onCreate()
 
-
+        DebugConfig().apply {
+            application = this@App
+            enableStrictMode = false
+            detectActivityLifecycle = true
+            detectFragmentLifecycle = true
+            logLevel = DebugHelper.LogLevel.E
+        }.also {
+            DebugHelper.setup(it)
+        }
     }
 
 }
